f1ca3e6d4b3fb11d2e93372c0efc8072b1e8309a
HerokuSL <ddilisha78@gmail.com>
2024-10-21 07:48:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_MSG: process.env.ALIVE_MSG || "Hi iam back now",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/cad7038fe82e47f79c609.jpg",
+MONGODB: process.env.MONGODB || "Put Mongodb Url",
 };
