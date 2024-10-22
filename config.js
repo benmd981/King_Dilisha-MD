79d272636e59ee94c073e3b419408db4cf494f8f
benmd981 <vnimsara492@gmail.com>
2024-10-22 14:27:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID||"Nj8CHYrJ#VXFx9sg5sWlNoXVXAkjJOxO3d5GYe3VRN2iQCyaek4c",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hi iam back now",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/cad7038fe82e47f79c609.jpg",
-MONGODB: process.env.MONGODB || "Put Mongodb Url",
 };
